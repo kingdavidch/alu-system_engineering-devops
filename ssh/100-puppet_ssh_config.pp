023de926cd145cc16cfe044184c85dfbdaec9710
KINGDAVID CHUKWUMERE <132105674+kingdavidch@users.noreply.github.com>
2023-11-29 19:10:54 +0200

    Update 100-puppet_ssh_config.pp

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,19 @@
-# Configures ssh for dev
+# Using puppet
+# 1. Your SSH client configuration must be configured to use the private key ~/.ssh/school
+# 2. Your SSH client configuration must be configured to refuse to authenticate using a password
+
 include stdlib
 
-file_line { 'no_password_auth':
-  ensure  => present,
+file_line { 'Refuse auth with password':
+  ensure => present,
   path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
   replace => true,
 }
 
-file_line { 'identity_file':
+file_line { 'specify identity file path':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/school',
+  line    => '     IdentityFile ~/.ssh/school',
   replace => true,
 }
